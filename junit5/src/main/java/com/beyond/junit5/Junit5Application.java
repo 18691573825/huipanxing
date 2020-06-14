7e66055f0e503f8d9f4234bfb144d7b7e80128f2
xiaohuihui <xiaohuihui@163.com>
2020-06-14 09:06:16 +0800

    添加主启动类

--- junit5/src/main/java/com/beyond/junit5/Junit5Application.java
+++ junit5/src/main/java/com/beyond/junit5/Junit5Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Junit5Application {
 
     public static void main(String[] args) {
+        //主启动类
         SpringApplication.run(Junit5Application.class, args);
     }
 
